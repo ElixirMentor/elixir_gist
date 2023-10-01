76582b1c78de4d518475f56a6def6f65f0bbcb81
Jacob Luetzow <jacob@perfectform.io>
2023-09-30 19:55:05 -0600

    submit

--- lib/elixir_gist/accounts/user.ex
+++ lib/elixir_gist/accounts/user.ex
@@ -8,7 +8,7 @@ defmodule ElixirGist.Accounts.User do
     field :password, :string, virtual: true, redact: true
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
-
+    has_many :gists, ElixirGist.Gists.Gist
     timestamps()
   end
 
