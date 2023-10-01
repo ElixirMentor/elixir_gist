4c8f35388aaa37128018b20d864620e49dfcce30
Jacob Luetzow <jacob@perfectform.io>
2023-09-30 20:46:28 -0600

    form submit and validation

--- lib/elixir_gist/gists/gist.ex
+++ lib/elixir_gist/gists/gist.ex
@@ -18,6 +18,6 @@ defmodule ElixirGist.Gists.Gist do
   def changeset(gist, attrs) do
     gist
     |> cast(attrs, [:name, :description, :markup_text, :user_id])
-    |> validate_required([:name, :description, :markup_text, :user_id])
+    |> validate_required([:name, :user_id])
   end
 end
