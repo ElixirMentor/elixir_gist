70fb5ec148044607ae0fa64dc5367a546774a8f9
Jacob Luetzow <jacob@perfectform.io>
2024-01-16 13:23:15 -0700

    copy and delete

--- assets/js/app.js
+++ assets/js/app.js
@@ -114,6 +114,21 @@ Hooks.UpdateLineNumbers = {
 
 };
 
+Hooks.CopyToClipboard = {
+    mounted() {
+        this.el.addEventListener("click", e => {
+            const textToCopy = this.el.getAttribute("data-clipboard-gist");
+            if (textToCopy) {
+                navigator.clipboard.writeText(textToCopy).then(() => {
+                    console.log("Gist copied to clipboard")
+                }).catch(err => {
+                    console.error("Failed to copy text: ", err)
+                })
+            }
+        })
+    }
+};
+
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks})
 
 // Show progress bar on live navigation and form submits
