d507106c0c302f13020eece824b17a47d79d9213
Jacob Luetzow <jacob@perfectform.io>
2024-01-29 14:52:11 -0700

    refactored create gist form to be a live component

---
+++ lib/elixir_gist_web/live/gist_form_component.ex
@@ -0,0 +1,84 @@
+defmodule ElixirGistWeb.GistFormComponent do
+  use ElixirGistWeb, :live_component
+  import Phoenix.HTML.Form
+
+  alias ElixirGist.{Gists, Gists.Gist}
+
+  def mount(socket) do
+    socket =
+      assign(
+        socket,
+        form: to_form(Gists.change_gist(%Gist{}))
+      )
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.form for={@form} phx-submit="create" phx-change="validate" phx-target={@myself}>
+        <div class="justify-center px-28 w-full space-y-4 mb-10">
+          <.input
+            field={@form[:description]}
+            placeholder="Gist description.."
+            autocomplete="off"
+            phx-debounce="blur"
+          />
+          <div>
+            <div class="flex p-2 items-center bg-emDark rounded-t-md border">
+              <div class="w-[300px] mb-2">
+                <.input
+                  field={@form[:name]}
+                  placeholder="Filename including extension..."
+                  autocomplete="off"
+                  phx-debounce="blur"
+                />
+              </div>
+            </div>
+            <div id="gist-wrapper" class="flex w-full" phx-update="ignore">
+              <textarea id="line-numbers" class="line-numbers rounded-bl-md" readonly>
+          <%= "1\n" %>
+        </textarea>
+              <%= textarea(@form, :markup_text,
+                id: "gist-textarea",
+                phx_hook: "UpdateLineNumbers",
+                class: "textarea w-full rounded-br-md",
+                placeholder: "Insert code...",
+                spellcheck: "false",
+                autocomplete: "off",
+                phx_debounce: "blur"
+              ) %>
+            </div>
+          </div>
+          <div class="flex justify-end">
+            <.button class="create_button" phx-disable-with="Creating...">Create gist</.button>
+          </div>
+        </div>
+      </.form>
+    </div>
+    """
+  end
+
+  def handle_event("validate", %{"gist" => params}, socket) do
+    changeset =
+      %Gist{}
+      |> Gists.change_gist(params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :form, to_form(changeset))}
+  end
+
+  def handle_event("create", %{"gist" => params}, socket) do
+    case Gists.create_gist(socket.assigns.current_user, params) do
+      {:ok, gist} ->
+        socket = push_event(socket, "clear-textareas", %{})
+        changeset = Gists.change_gist(%Gist{})
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, push_navigate(socket, to: ~p"/gist?#{[id: gist]}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
+  end
+end
