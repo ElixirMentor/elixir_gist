1a842b61795f2c3c4be380e7de04c1622b43d2bf
Jacob Luetzow <jacob@perfectform.io>
2024-02-19 16:04:07 -0700

    adjusted our endpoint config for cors

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,9 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :elixir_gist, ElixirGistWeb.Endpoint,
+  url: [host: "https://elixir-gist.gigalixirapp.com/", scheme: "https", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  check_origin: ["https://elixir-gist.gigalixirapp.com/"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
