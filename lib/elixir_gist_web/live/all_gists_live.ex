c17424c5c14d60b083dded493728a4067b1e1f63
Jacob Luetzow <jacob@perfectform.io>
2024-03-07 11:06:45 -0700

    cleaned up html

--- lib/elixir_gist_web/live/all_gists_live.ex
+++ lib/elixir_gist_web/live/all_gists_live.ex
@@ -9,36 +9,23 @@ defmodule ElixirGistWeb.AllGistsLive do
 
   def gist(assigns) do
     ~H"""
-    <div
-      class="justify-center px-28 w-full mb-20"
-      style="cursor: pointer;"
-      phx-click="select"
-      phx-value-id={@gist.id}
-    >
+    <div class="justify-center px-28 w-full mb-20" phx-click="select" phx-value-id={@gist.id}>
       <div class="flex justify-between mb-4">
-        <div class="flex item-center ml-10">
-          <img
-            src="/images/user-image.svg"
-            alt="Profile Image"
-            class="round-image-padding w-8 h-8 mb-6"
-          />
+        <div class="flex items-center ml-10">
+          <img src="/images/user-image.svg" alt="Profile Image" class="rounded-full w-8 h-8 mb-6" />
           <div class="flex flex-col ml-4">
             <div class="font-bold text-base text-emLavender-dark">
               <%= @current_user.email %><span class="text-white">/</span><%= @gist.name %>
             </div>
             <div class="text-lg text-white font-bold"><%= @gist.updated_at %></div>
-            <p class="text-left text-sm text-white font-brand"><%= @gist.description %></p>
+            <p class="text-sm text-white"><%= @gist.description %></p>
           </div>
         </div>
-        <div class="flex item-center mr-10">
+        <div class="flex items-center mr-10">
           <img src="/images/comment.svg" alt="Comment Count" class="h-6" />
-          <div class="flex items-center text-white font-brand h-6 px-1">
-            0
-          </div>
+          <span class="text-white h-6 px-1">0</span>
           <img src="/images/BookmarkOutline.svg" alt="Save Count" class="h-6" />
-          <div class="flex items-center text-white font-brand h-6 px-1">
-            0
-          </div>
+          <span class="text-white h-6 px-1">0</span>
         </div>
       </div>
       <div class="justify-center px-10 mb-10">
